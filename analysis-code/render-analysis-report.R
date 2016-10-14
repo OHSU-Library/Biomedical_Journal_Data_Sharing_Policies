2e05c78af86353542c05c55b6e67416e53379f0e
jminnier <jessica.minnier@gmail.com>
2016-10-13 20:07:23 -0700

    updated analyses to better align with current manuscript, added several tables.

--- analysis-code/render-analysis-report.R
+++ analysis-code/render-analysis-report.R
@@ -2,10 +2,10 @@
 ## Run this file to render both a report with code and a report without code.
 ## ============================================================================ ##
 
-rmarkdown::render("analysis-datasharing.Rmd",
+rmarkdown::render("analysis-code/analysis-datasharing.Rmd",
                   output_file = "analysis-datasharing.html", # this is relative to the markdown file
                   params = list(showcode=FALSE))
 
-rmarkdown::render("analysis-datasharing.Rmd",
+rmarkdown::render("analysis-code/analysis-datasharing.Rmd",
                   output_file = "analysis-datasharing-withcode.html",  # this is relative to the markdown file
                   params = list(showcode=TRUE))
